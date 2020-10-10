eedb39c691d82aadb74e988ade87e2ce964c8111
Gilbert Emerson <emer7@users.noreply.github.com>
2020-10-10 10:16:23 +0800

    Updates

---
+++ precache-manifest.03eab5e4e33b33210d6ee7803dd41d9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e8f43ff5e32edbfb7952e99d2ddd19",
+    "url": "/c4g-magic-bible/index.html"
+  },
+  {
+    "revision": "20b397a9a1eb4788c9b6",
+    "url": "/c4g-magic-bible/static/css/2.0939e70c.chunk.css"
+  },
+  {
+    "revision": "d6a17949e9edb3ebb0a3",
+    "url": "/c4g-magic-bible/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "20b397a9a1eb4788c9b6",
+    "url": "/c4g-magic-bible/static/js/2.f43deefc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/c4g-magic-bible/static/js/2.f43deefc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6a17949e9edb3ebb0a3",
+    "url": "/c4g-magic-bible/static/js/main.9af9243f.chunk.js"
+  },
+  {
+    "revision": "326b42990b1b4d9aca50",
+    "url": "/c4g-magic-bible/static/js/runtime-main.2d181715.js"
+  }
+]);
